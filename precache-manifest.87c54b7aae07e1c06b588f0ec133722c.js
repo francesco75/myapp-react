5b410065a065f5cf1f775bac392f7c246ae96167
FRSCO <frscollo@hotmail.it>
2020-05-20 11:52:06 +0100

    Updates

---
+++ precache-manifest.87c54b7aae07e1c06b588f0ec133722c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cebdde98a0590e436b3168ac2445f389",
+    "url": "/myapp-react/index.html"
+  },
+  {
+    "revision": "ef3c967024a2967688ac",
+    "url": "/myapp-react/static/css/main.c5c1d047.chunk.css"
+  },
+  {
+    "revision": "f173ea738d2266ceb39a",
+    "url": "/myapp-react/static/js/2.d6e2ceb0.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/myapp-react/static/js/2.d6e2ceb0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef3c967024a2967688ac",
+    "url": "/myapp-react/static/js/main.9f03e042.chunk.js"
+  },
+  {
+    "revision": "9fd071da5c05f4de73f8",
+    "url": "/myapp-react/static/js/runtime-main.780e9c71.js"
+  }
+]);
